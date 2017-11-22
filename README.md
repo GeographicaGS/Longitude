14a9c3ae21c6cf3619353df9e230996792d77903
Alberto Asuero <alberto@geographica.gs>
2017-11-22 12:33:02 +0100

    0.1.5

---
+++ README.md
@@ -0,0 +1,8 @@
+# Longitude
+
+A bunch of middleware functions to build applications on top of CARTO.
+
+How to use:
+```
+pip install geographica-longitude
+```
--- setup.py
+++ setup.py
@@ -7,18 +7,17 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 # get the requirements
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-print(find_packages(exclude=['contrib', 'docs', 'tests']))
 setup(
     name='geographica-longitude',
 
-    version='0.1.4',
+    version='0.1.5',
 
     description='Longitude',
     long_description=long_description,
