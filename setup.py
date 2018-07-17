d6a025fec5e00f61a07503d70f1a97fe23a6f0e0
Alejandro Hall <alejandro@geographica.gs>
2018-07-17 12:55:20 +0200

    Add retries feature in queries

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('requirements.txt') as f:
 setup(
     name='geographica-longitude',
 
-    version='0.1.22',
+    version='0.1.23',
 
     description='Longitude',
     long_description=long_description,
