d25caf15ed1afc13be1c847d1b9a5ad9c63109f3
Alberto Asuero <alberto@geographica.gs>
2018-07-27 11:19:13 +0200

    0.1.26

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('requirements.txt') as f:
 setup(
     name='geographica-longitude',
 
-    version='0.1.24',
+    version='0.1.26',
 
     description='Longitude',
     long_description=long_description,
