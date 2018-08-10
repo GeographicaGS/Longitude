074db3a75fc1b77b66f14d2c10cb6bdab2879f25
Alberto Asuero <alberto@geographica.gs>
2018-08-10 18:25:45 +0200

    v0.1.28

--- longitude/auth.py
+++ longitude/auth.py
@@ -74,6 +74,9 @@ def auth():
 
     return decorator
 
+def check_login_fields(fields_list, user_data, username):
+    check_list = [True for x in fields_list if user_data[x] == username]
+    return True if check_list else False
 
 @routes.route('/token', methods=['GET'])
 def get_token():
@@ -94,18 +97,9 @@ def get_token():
     })
     user_data = user_model.get_user(username)
 
-    log = logging.getLogger()
-
-    def check_login_fields(fields_list):
-        check_list = [True for x in fields_list if user_data[x] == username]
-        if check_list:
-            return True
-        else:
-            return False
-
     login_fields = cfg['AUTH_LOGIN_FIELDS'].split(',')
 
-    if not user_data or not check_login_fields(login_fields) or not bcrypt.checkpw(password.encode('utf8'),
+    if not user_data or not check_login_fields(login_fields, user_data, username) or not bcrypt.checkpw(password.encode('utf8'),
                                                                                 user_data['password'].encode('utf-8')):
         return jsonify({'msg': 'Bad username or password'}), 401
 
