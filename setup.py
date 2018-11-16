8012e4dae11679892e05e0c51889d27b64e1f959
Roberto García Calero <roberto.garcia@geographica.gs>
2018-11-16 20:09:20 +0100

    Bump version to v0.2.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('requirements.txt') as f:
 setup(
     name='geographica-longitude',
 
-    version='0.1.31',
+    version='0.2.0',
 
     description='Longitude',
     long_description=long_description,
